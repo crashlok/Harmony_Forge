10c512c858851de7769c28621b86971bff15763c
crashlok <crashlok@users.noreply.github.com>
2023-10-10 17:23:02 +0200

    playing notes with rodio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
-}
+
+    use rodio::{
+        source::{SineWave, Empty, Source,TakeDuration},
+        OutputStream, Sink,
+    };
+    use std::time::Duration;
+    
+    fn main() {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+    
+ 
+        let source = SineWave::new(440.0)
+            .take_duration(Duration::from_secs_f32(0.25))
+            .amplify(0.20);
+        sink.append(source);
+    
+        let source = SineWave::new(0.).take_duration(Duration::from_secs_f32(0.25));
+        sink.append(source);
+
+        sink.sleep_until_end();
+    }
+    
+
