ce900545041f264db02fbfbf0a100ea60a8b7a33
crashlok <crashlok@users.noreply.github.com>
2023-10-12 12:49:23 +0200

    note module for easier source creation and triyng to poly with mix

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
+use rodio::{
+    OutputStream, Sink,Source
+};
 
-    use rodio::{
-        source::{SineWave, Empty, Source,TakeDuration},
-        OutputStream, Sink,
-    };
-    use std::time::Duration;
-    
-    fn main() {
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let sink = Sink::try_new(&stream_handle).unwrap();
-    
- 
-        let source = SineWave::new(440.0)
-            .take_duration(Duration::from_secs_f32(0.25))
-            .amplify(0.20);
-        sink.append(source);
-    
-        let source = SineWave::new(0.).take_duration(Duration::from_secs_f32(0.25));
-        sink.append(source);
+use harmony_forge::note;
+use std::time::Duration;
 
-        sink.sleep_until_end();
-    }
+const A:f32 = 440.0;
+
+fn main() {
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    sink.append(note::sine_wave_octave(A, 0.25,-1));
+    sink.append(note::pause(0.25));
+    sink.append(note::sine_wave_octave(A, 0.25,1).reverb(Duration::from_secs_f32(0.5),0.1));
+    sink.append(note::pause(1.));
+    sink.append(note::sine_wave_octave(A, 0.5, 0));
+    sink.append(note::sine_wave_octave(A, 0.5,-1).mix(note::sine_wave_octave(A, 0.5,0)));
+
+    sink.sleep_until_end();
+}
     
 
