0f5c89038217b0e46462464220d53c71f748fe9a
crashlok <crashlok@users.noreply.github.com>
2023-10-18 16:42:51 +0200

    Scales "better" organisation and starting to fade out

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,5 @@
-pub mod note{
-    use rodio::{source::{TakeDuration, SineWave,Mix}, Source};
-    use std::time::Duration;
 
-    //pub fn poly<S,S1,S2>(sources:Vec<S>)-> Mix<S1,S2>
-    //where
-    //S:Source,
-    //S1:Source,
-    //S2:Source,
-    //{}
-    fn build_scale(){
-
-    }
+mod source_fadeout;
 
 
-    pub fn octave(x:f32,o:i32)->f32{
-        x*(2.0_f64.powi(o)as f32)
-    }
-
-    pub fn sine_wave_octave(freq:f32,duration:f32,o:i32)->TakeDuration<SineWave>{
-        SineWave::new(octave(freq,o)).take_duration(Duration::from_secs_f32(duration))
-    }
-
-    pub fn sine_wave(freq:f32,duration:f32)->TakeDuration<SineWave>{
-        SineWave::new(freq).take_duration(Duration::from_secs_f32(duration))
-    }
-
-    pub fn pause(duration:f32)-> TakeDuration<SineWave>{
-        sine_wave(0., duration)
-    }
-
-    #[cfg(test)]
-    mod tests{
-        use super::*;
-        
-        #[test]
-        fn octaviert(){
-            assert_eq!(octave(440.0,-1),220.0);
-            assert_eq!(octave(440.0,1),880.0);
-            assert_eq!(octave(440.0,0),440.0);
-        }
-    }
-
-}
+pub mod note; 
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
-use rodio::{
-    OutputStream, Sink,Source
-};
+use rodio::{OutputStream, Sink};
 
-use harmony_forge::note;
-use std::time::Duration;
+use harmony_forge::note::{self, Scale};
 
-const A:f32 = 440.0;
+const A: f32 = 440.0;
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
 
-    sink.append(note::sine_wave_octave(A, 0.5,-1).mix(note::sine_wave(A+(2.0/3.0*A), 0.5)));
-    sink.append(note::pause(0.25));
-    sink.append(note::sine_wave_octave(A, 0.25,1).reverb(Duration::from_secs_f32(0.5),0.1));
-    sink.append(note::pause(1.));
-    sink.append(note::sine_wave_octave(A, 0.5, 0));
-    sink.append(note::sine_wave_octave(A, 0.5,-1).mix(note::sine_wave_octave(A, 0.5,0)).mix(note::sine_wave_octave(A, 0.5,1)));
+    sink.set_volume(0.25);
+    let scale_a_major: Scale = Scale::new_major(A);
+    for s in scale_a_major.as_freq() {
+        sink.append(note::sine_wave_octave(s, 0.25, 0))
+    }
 
     sink.sleep_until_end();
 }
-    
 
