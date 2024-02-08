eca347840dfe7fe000d31e1132ed462e0b1d6422
crashlok <crashlok@users.noreply.github.com>
2024-02-08 10:20:52 +0100

    made the multiple add gen

--- src/generators/universal_generator.rs
+++ src/generators/universal_generator.rs
@@ -21,3 +21,47 @@ impl<I, C: Fn(&Models) -> usize> Generator for MultipleClosure<I, C> {
         (*(self.generators[index])).gen(gen_models)
     }
 }
+
+fn add_arrays<const N: usize, T: Default + Copy>(mut input: Vec<[T; N]>) -> [T; N]
+where
+    for<'a> &'a mut T: std::ops::AddAssign<&'a T>,
+{
+    return match input.len() {
+        0 => [T::default(); N],
+        1 => *input.first().unwrap(),
+        _ => add_arrays({
+            let input2 = input.clone();
+            input
+                .first_mut()
+                .unwrap()
+                .iter_mut()
+                .zip(input2.last().unwrap())
+                .for_each(|(mut a, b)| a += b);
+            input.pop();
+            input
+        }),
+    };
+}
+struct MultipleAdd<const N: usize, I> {
+    generators: Vec<Box<Gen<[I; N]>>>,
+}
+impl<const N: usize, I> MultipleAdd<N, I> {}
+
+impl<const N: usize, I> Generator for MultipleAdd<N, I>
+where
+    for<'a> &'a mut I: std::ops::AddAssign<&'a I>,
+    I: Default + Copy,
+{
+    type Item = [I; N];
+    fn gen(&mut self, gen_models: Models) -> (Self::Item, Models) {
+        let (results, new_models) =
+            self.generators
+                .iter_mut()
+                .fold((Vec::new(), gen_models), |(mut res, models), gen| {
+                    let (re, models) = gen.gen(models);
+                    res.push(re);
+                    (res, models)
+                });
+        (add_arrays(results), new_models)
+    }
+}
