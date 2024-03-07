3594157c54e6ec027da0731bb7cc4163548f09ec
crashlok <crashlok@users.noreply.github.com>
2024-03-07 09:43:42 +0100

    optimizations and examples

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![deny(warnings)]
 #![allow(unused_imports)]
+use crate::plays::*;
+use dialoguer::FuzzySelect;
 use harmony_forge::{
     bpm_to_micros_per_beat,
     generators::{
@@ -16,80 +18,34 @@ use midir::{ConnectError, MidiOutput, MidiOutputConnection, MidiOutputPort};
 use midly::num::u7;
 use nodi::{self, timers::Ticker};
 use std::{sync::mpsc, thread, time::Duration};
-use dialoguer::FuzzySelect;
+
+mod plays;
 
 fn main() {
-    let con = choose_connection(midir::MidiOutput::new("harmony_forge").expect("failed to instanciat output")).expect("failed to connect");
-
-    let (_tx, handle): (mpsc::Sender<()>, thread::JoinHandle<()>) = MusicGenerator::new()
-        .add_generator(Box::new(MultipleClosure::new(
-            |models| match models.time.get_bars() {
-                0 => 1,
-                _ => 1,
-            },
-            vec![
-                Box::new(OnClosurePattern::new(
-                    |models| models.time.on_eight() && models.time.get_eights_i32() % 3 == 0,
-                    NotesDependingBar::new(vec![
-                        Chord::new_major(56).as_midi_notes(),
-                        Chord::new_minor(53).as_midi_notes(),
-                        Chord::new_minor(48).as_midi_notes(),
-                        Chord::new_minor(48).as_midi_notes(),
-                    ]),
-                    0,
-                )),
-                Box::new(OnBeatPattern::new(
-                    NotesDependingBar::new(vec![
-                        Chord::new_major(56).as_midi_notes(),
-                        Chord::new_minor(53).as_midi_notes(),
-                        Chord::new_minor(48).as_midi_notes(),
-                        Chord::new_minor(48).as_midi_notes(),
-                    ]),
-                    0,
-                )),
-            ],
-        )))
-        .add_generator(Box::new(MultipleClosure::new(
-            |models| (models.time.get_bars() % 3) as usize,
-            vec![
-                Box::new(OnClosurePattern::new(
-                    |models| {
-                        models.time.on_eight()
-                            && !models.time.on_quarter()
-                            && models.time.get_eights_i32() % 3 == 0
-                    },
-                    OneNote::new(vec![61]),
-                    1,
-                )),
-                Box::new(OnClosurePattern::new(
-                    |models| models.time.on_eight() && !models.time.on_quarter(),
-                    OneNote::new(vec![61]),
-                    1,
-                )),
-            ],
-        )))
-        .add_generator(Box::new(OnClosurePattern::new(
-            |models| models.time.on_eight(),
-            NearNotes::new(Scale::new_minor(60), 1..3),
-            2,
-        )))
-        .add_generator(Box::new(OnClosurePattern::new(
-            |models| models.time.on_bar(),
-            NotesDependingBar::new(vec![
-                vec![octave(Chord::new_major(56).as_midi_notes()[0], -1)],
-                vec![octave(Chord::new_major(53).as_midi_notes()[0], -1)],
-                vec![octave(Chord::new_major(48).as_midi_notes()[0], -1)],
-                vec![octave(Chord::new_major(48).as_midi_notes()[0], -1)],
-            ]),
-            3,
-        )))
-        .play(
-            Ticker::with_initial_tempo(100, bpm_to_micros_per_beat(120)),
-            con,
-            4,
-        );
+    let con = choose_connection(
+        midir::MidiOutput::new("harmony_forge").expect("failed to instanciat output"),
+    )
+    .expect("failed to connect");
+    let choosen_play = choose_play(vec![
+        (piece, "piece"),
+        (random,"random")
+    ]);
+    let (_tx, handle): (mpsc::Sender<()>, thread::JoinHandle<()>) = choosen_play().play(
+        Ticker::with_initial_tempo(100, bpm_to_micros_per_beat(120)),
+        con,
+        4,
+    );
 
     handle.join().expect("music generator paniced")
+}
+
+fn choose_play(plays: Vec<(fn() -> MusicGenerator, &str)>) -> fn() -> MusicGenerator {
+    let play_index = FuzzySelect::new()
+        .with_prompt("choose play")
+        .items(&(plays.iter().map(|f| f.1).collect::<Vec<&str>>()))
+        .interact()
+        .expect("failed to choose");
+    plays[play_index].0
 }
 
 fn _probability_test() {
@@ -102,18 +58,20 @@ fn _probability_test() {
         println!();
     }
 }
-fn choose_connection(out:MidiOutput)-> Result<MidiOutputConnection, ConnectError<MidiOutput>>{
+fn choose_connection(out: MidiOutput) -> Result<MidiOutputConnection, ConnectError<MidiOutput>> {
     println!();
-    let port_names = out.ports().iter().map(|port|out.port_name(port).unwrap_or("<no device name>".to_owned())).collect::<Vec<String>>();
-    let choosen_port_index =
-        FuzzySelect::new()
-             .with_prompt("choose port")
-             .items(&port_names)
-             .interact()
-             .expect("failed to choose");
+    let port_names = out
+        .ports()
+        .iter()
+        .map(|port| out.port_name(port).unwrap_or("<no device name>".to_owned()))
+        .collect::<Vec<String>>();
+    let choosen_port_index = FuzzySelect::new()
+        .with_prompt("choose port")
+        .items(&port_names)
+        .interact()
+        .expect("failed to choose");
 
     let port: &MidiOutputPort = &out.ports()[choosen_port_index];
     println!();
     out.connect(port, "HarmonyForgeOut")
 }
-
--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,5 @@
 use midir::MidiOutputConnection;
 use nodi::{timers::Ticker, Event, Moment, Timer};
-use std::time::Instant;
 
 #[derive()]
 pub struct Player<I>
@@ -27,13 +26,9 @@ where
     pub fn play(&mut self) {
         let mut counter = 0_u32;
         loop {
-            let timer = Instant::now();
-
             let moment: Moment = (*self.generator).next().unwrap();
 
             if !moment.is_empty() {
-                println!("{:?}", timer.elapsed());
-
                 self.timer.sleep(counter);
                 counter = 0;
 
